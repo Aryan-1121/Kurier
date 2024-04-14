e8ad2aea0ae0ebc63ac7a750aac7c300ad4ff767
Aryan <aryanmaurya879@gmail.com>
2024-04-14 10:42:52 +0530

    added dotenv & changed type to module

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-const express = require('express')
+// const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+// const dotenv = require('dotenv');               // to get dotenv configuration 
+
+
 
 const app  = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+
 
 app.get('/', (req,res)=>{
     res.send('HEYY THERE !!! ')
 })
 
-app.listen(5000, () =>{
-    console.log('app listening to port 5000');
+app.listen(PORT, () =>{
+    console.log(`app listening to port ${PORT}`);
 })
 
