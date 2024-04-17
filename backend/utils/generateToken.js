2c7a5a3dc323a076de7b1f88e8a881e0a81e02aa
Aryan <aryanmaurya879@gmail.com>
2024-04-17 18:38:09 +0530

    added secure option while setting cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -11,6 +11,8 @@ const generateTokenAndSetCookie = (userId, res) => {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS  (-> 15 days)
 		httpOnly: true, // to prevent XSS attacks cross-site scripting attacks(javascript)
 		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development",
+
 	});
 };
 
