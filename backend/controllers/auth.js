da2c71dc0728f16be093b60047bdb465f208c92c
Aryan <aryanmaurya879@gmail.com>
2024-04-17 18:16:24 +0530

    added one more check before pushing to DB

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -5,19 +5,19 @@ export const signup = async (req, res) => {
     try {
         const { fullName, userName, password, confirmPassword, gender } = req.body;
 
-        if(password !== confirmPassword){
+        if (password !== confirmPassword) {
             return res.status(400).json({
-                error:"Password & confirmPassword didn't match "
+                error: "Password & confirmPassword didn't match "
             });
         }
         // check if userName already exists (needs to be unique)
-        const user = await User.findOne({userName});
-        if(user){
+        const user = await User.findOne({ userName });
+        if (user) {
             res.status(400).json({
-                error :'Username already exist'
+                error: 'Username already exist'
             })
         }
-        
+
         //     TODO: hash password here 
 
         const salt = await bcrypt.genSalt(10);
@@ -26,7 +26,7 @@ export const signup = async (req, res) => {
         // RANDOM PROFILE PIC (using this free api - https://avatar.iran.liara.run/)
         // for male  -> https://avatar.iran.liara.run/public/boy?username=[value]
         // for female  -> https://avatar.iran.liara.run/public/girl?username=[value]
-        
+
         const maleDP = `https://avatar.iran.liara.run/public/boy?username=${userName}`
         const femaleDP = `https://avatar.iran.liara.run/public/girl?username=${userName}`
 
@@ -37,17 +37,24 @@ export const signup = async (req, res) => {
             userName,
             password: hashedPassword,
             gender,
-            profilePic: gender=== 'male' ? maleDP : femaleDP  
+            profilePic: gender === 'male' ? maleDP : femaleDP
         })
 
-        await newUser.save();
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            userName: newUser.userName,
-            profilePic: newUser.profilePic
+        //  if newUser got created succesfully then save it to database 
+        if (newUser) {
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                userName: newUser.userName,
+                profilePic: newUser.profilePic
 
-        })
+            })
+        } else {
+            res.status(400).json({
+                error: 'Invalid user data'
+            })
+        }
 
 
     } catch (error) {
