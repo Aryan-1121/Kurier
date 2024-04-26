3441bce568194e5f14da216073b873446c440763
Aryan <aryanmaurya879@gmail.com>
2024-04-26 17:15:37 +0530

    created controller to fetch all user from db and return for sidebar

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,8 @@
 
 
 import express from 'express';
+import protectRoute from '../middleware/protectRoute.js';
+import { getUsersForSidebar } from '../controllers/userController.js';
 
 
 const router = express.Router();
