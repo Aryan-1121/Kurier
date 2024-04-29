78d790df7b507f67c8229f885ebddc7de1ef173b
Aryan <aryanmaurya879@gmail.com>
2024-04-30 00:57:15 +0530

    added conversation component

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ import Conversations from './Conversations';
 
 const Sidebar = () => {
   return (
-    <div className='border-r border-slate-500 p-4 flex flex-col'>
+    <div className='border-r border-yellow-800 p-4 flex flex-col'>
       <SearchInput />
       <div className='divider px-3'></div>       
 
