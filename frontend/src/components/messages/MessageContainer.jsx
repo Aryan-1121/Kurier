c32f94d006cbb88c7db77a5554c2c8504c77918c
Aryan <aryanmaurya879@gmail.com>
2024-04-30 12:37:00 +0530

    Completed Sidebar comp. Started with MessageContainer comp.

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// this component will also have 3 other components -> 1. Header (to whom you are messaging) , 2. Messages (each message) , 3. MessageInput
+
+const MessageContainer = () => {
+  return (
+    <div>MessageContainer</div>
+  )
+}
+
+export default MessageContainer
