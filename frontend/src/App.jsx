bd4716267726b894260a9c5e81ca59f042eee1d4
Aryan <aryanmaurya879@gmail.com>
2024-04-30 17:02:26 +0530

    onClick switching b/w login and sign-up page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
-// import Signup from "./pages/signup/Signup"
+import Signup from "./pages/signup/Signup"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
    <div className="p-4 h-screen flex items-center justify-center">
-      {/* <Signup /> */}
-      <Home />
+
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+      </Routes>
+
    </div>
   )
 }
