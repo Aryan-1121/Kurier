127d6dd72b46d40e3a0c9f3aa5405f54b0653d37
Aryan <aryanmaurya879@gmail.com>
2024-05-01 02:03:16 +0530

    redirection to login/signup/home page based on weather user is authenticated/logged-in or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,23 @@
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
 import SignUp from "./pages/signup/SignUp"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
+import { useAuthContext } from "./context/AuthContext"
 
 function App() {
+  const { authenticatedUser } = useAuthContext();
 
   return (
    <div className="p-4 h-screen flex items-center justify-center">
 
-
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        {/* if user is authenticated then be at home page else redirect to login page */}
+        <Route path="/" element={authenticatedUser ? <Home /> : <Login />} />
+        {/* if user is authenticated then redirect to home page else redirect to login page */}
+        <Route path="/login" element={authenticatedUser ? <Navigate to='/' /> : <Login />} />
+        {/* if user is authenticated then redirect to home page else redirect to signup page */}
+        <Route path="/signup" element={authenticatedUser ? <Navigate to='/' /> : <SignUp />} />
 
       </Routes>
       <Toaster />
