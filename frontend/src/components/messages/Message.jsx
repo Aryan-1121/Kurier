f4ea548f009e1c2d54bdae1350f7a1795405e91e
Aryan <aryanmaurya879@gmail.com>
2024-05-02 21:39:49 +0530

    diffrentiating the msgs b/w me and the selectedPerson & based on that making changes in chatbg colour, prfilePic, chatBubble side(left/right)

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -12,21 +12,29 @@ const Message = ({ message }) => {
   // we are also saving senderId and receiverId in the message object in the backend
   const isMessageFromMe = authenticatedUser._id === message.senderId;
 
+  const chatBubbleClassName = isMessageFromMe ? 'chat chat-end' : 'chat chat-start';
+
+  const chatProfilePic = isMessageFromMe ? authenticatedUser.profilePic : selectedConversation.profilePic;
+
+  const chatBgColour = isMessageFromMe ? 'bg-blue-500' : '';
+
+
+
 
   return (
     // "chat-end" is used to place the message on the right side (when I send message) and "chat-start" is used to place the message on the left side (when other person sends message) coming from daisyui
-    <div className={`chat chat-end`}>
+    <div className={chatBubbleClassName}>
       {/* "chat-image avatar" is used to display the profile picture */}
       <div className='chat-image avatar'>
         <div className='w-10 rounded-full'>
           <img alt='Tailwind CSS chat bubble component'
-            src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
+            src={chatProfilePic}
           />
         </div>
       </div>
 
       {/* "chat-bubble" is used to display the message */}
-      <div className={`chat-bubble text-white bg-orange-800 `}>{message.message}</div>
+      <div className={`chat-bubble text-white bg-orange-800 ${chatBgColour} `}>{message.message}</div>
       {/* "chat-footer" -> we are using it to display the time below the message */}
       <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:54</div>
     </div>)
