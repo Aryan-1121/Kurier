d7912982307bdcd016b4bcf99b8a8c4d0ca3e264
Aryan <aryanmaurya879@gmail.com>
2024-05-02 12:48:48 +0530

    used name and profile pic coming from BE to show in the sideBarConversations

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Conversation from './Conversation'
+import { useGetSideBarConversations } from '../../hooks/useGetSideBarConversations';
 
 const Conversations = () => {
+
+  const { loading, conversations } = useGetSideBarConversations();
+  console.log(conversations);
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-      <Conversation />
-      <Conversation />
-      <Conversation />
 
-      <Conversation />
+
+      {conversations.map((conversation) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+        />
+      ))}
+
+
+
+      {/* while loading is true show a loading spinner, and when loading is false show nothing(null)  */}
+      {loading ? <span className='loading loading-spinner'></span> : null}
+
     </div>)
 }
 
